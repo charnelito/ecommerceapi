37d2a340cbdf19ee6c57b9caf04a6104230c6221
Charnel Catipay <ccatipay@gmail.com>
2021-10-11 14:27:53 +0800

    Understanding Node.js Routng

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("DBConnection Successfull!"))
+.catch((err) =>{
+    console.log(err);
+});
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+// app.get("/api/test", ()=>{
+//     console.log("test is successful")
+// })
+
+app.listen(process.env.PORT || 5000, () => {
     console.log('Backend server is running!');
 });
